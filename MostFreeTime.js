9f26de16431300d256fc9ce0a567103d103d3d8b
Thein-Naing <theinnaing0908@gmail.com>
2023-08-31 11:09:07 +0800

    first commit

---
+++ MostFreeTime.js
@@ -0,0 +1,81 @@
+/* Have the function MostFreeTime(strArr) read the strArr parameter being passed which will represent a full day 
+ and will be filled with events that span from time X to time Y in the day. 
+The format of each event will be hh:mmAM/PM-hh:mmAM/PM. 
+For example, strArr may be [“10:00AM-12:30PM”,”02:00PM-02:45PM”,”09:10AM-09:50AM”]. 
+Your program will have to output the longest amount of free time available between the start of your first event 
+and the end of your last event in the format: hh:mm. The start event should be the earliest event in the day 
+and the latest event should be the latest event in the day. 
+The output for the previous input would therefore be 01:30 
+(with the earliest event in the day starting at 09:10AM and the latest event ending at 02:45PM).
+The input will contain at least 3 events and the events may be out of order.*/
+  
+  
+  
+  //1. declare strArr for testing purpose
+let  strArr = ["12:15PM-02:00PM", "09:00AM-10:00AM", "10:30AM-12:00PM" ];
+
+const MostFreeTime = (strArr) => { 
+ 
+  // 2.create an empty array to store the minutes of each event
+  let minArr = [];
+  // create a variable to keep track of the longest free time: initailly zero
+  let longest = 0;
+  
+  // 3.function to convert a time string to minutes
+  const  ParseTime = (time) => {
+      let minutes = 0 
+      // add 12 hours (720 minutes) for pm times
+      // use regex string.mtach(regex) method
+      if(time.match(/pm/i)) {  //The regular expression includes the i flag so that upper/lower case differences will be ignored.
+          minutes += 12 * 60  // if time is pm times. minutes = minutes + ( 12 * 6)
+      }
+      // 4.add the hours converted to minutes
+      if(time.split(':')[0] !== '12') { // 11:59AM.split(:)[0] and here [0] = 11  time =[11, 59]
+         minutes += time.split(':')[0] * 60  // so  minutes = 11 * 60
+      }  
+      // 5.add the minutes
+      minutes += Number(time.split(':')[1].match(/[0-9][0-9]/)[0]) // here [1] = 59 of time[11]
+      return minutes
+  }
+
+ 
+  
+  // 6.loop through the array of events and convert each time to minutes and add to minutes array
+  for(let i = 0; i < strArr.length; i++) {
+       time1 = strArr[i].split('-')[0] //"09:00AM-10:00AM"
+       time2 = strArr[i].split('-')[1] // "10:30AM-12:00PM"
+      minArr.push([ParseTime(time1), ParseTime(time2)])
+  }    
+  
+//   // 7.sort the array of minutes in ascending order
+//   minArr.sort(function(a, b) {
+//       return a[0] - b[0]
+//   })
+  
+  // 8.loop through the sorted array and find the longest free time
+  for(let j = 0; j < minArr.length - 1; j++) {
+      if(longest < minArr[j + 1][0] - minArr[j][1]) {
+          longest = minArr[j + 1][0] - minArr[j][1]
+      }
+  }
+  
+  // 9.convert the longest free time to hours and minutes
+  let hours = 0
+  while(longest >= 60) {
+      longest -= 60;
+      hours ++
+  }
+  
+  // 10.add a leading zero for (single-digit minutes) and (single-digit) hours
+  if(hours.toString().length === 1) {
+      hours = "0" + hours   //01:10
+  }
+  if(longest.toString().length === 1) {
+      return hours + ":0" + longest    //00:01
+  } else {
+      return hours + ":" + longest   //11:25
+  }
+}
+
+// 11.keep this function call here 
+console.log(MostFreeTime(strArr));
